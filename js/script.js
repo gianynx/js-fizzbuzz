f9e852d7648f83904118857153f7bec51adbc7d0
Gianmarco Incocciati <minusgian@gmail.com>
2023-03-10 16:07:02 +0100

    add if / else conditions

--- js/script.js
+++ js/script.js
@@ -6,15 +6,18 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
 for (let a = 1; a <= 100; a++) {
     // console.log(a);
     
-    let fizzbuzz = document.getElementById('fizzbuzz');
+    let game = document.getElementById('game');
 
     if (a % 3 === 0 && a % 5 === 0) {
-        fizzbuzz.innerHTML = `FizzBuzz`;
+        let fizzbuzz = document.querySelector('.fizzBuzz');
+        fizzbuzz.innerHTML += `FizzBuzz`;
     } else if (a % 3 === 0) {
-        fizzbuzz.innerHTML = `Fizz`;
+        let fizz = document.querySelector('.fizz');
+        fizz.innerHTML += `Fizz`;
     } else if (a % 5 === 0) {
-        fizzbuzz.innerHTML = `Buzz`;
+        let buzz = document.querySelector('.buzz');
+        buzz.innerHTML += `Buzz`;
     } else {
-        fizzbuzz.innerHTML = a;
+        game.innerHTML += a;
     }
 }
